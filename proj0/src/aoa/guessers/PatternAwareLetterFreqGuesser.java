33d553cf6904fecdd800102edfa647e0797d480d
lmagana5 <luis.a.magana1@gmail.com>
2023-08-20 18:21:21 -0700

    Complete PatternAwareLetterFrequency

--- proj0/src/aoa/guessers/PatternAwareLetterFreqGuesser.java
+++ proj0/src/aoa/guessers/PatternAwareLetterFreqGuesser.java
@@ -1,8 +1,8 @@
 package aoa.guessers;
 
 import aoa.utils.FileUtils;
-import java.util.List;
-import java.util.Map;
+
+import java.util.*;
 
 public class PatternAwareLetterFreqGuesser implements Guesser {
     private final List<String> words;
@@ -15,8 +15,68 @@ public class PatternAwareLetterFreqGuesser implements Guesser {
     /** Returns the most common letter in the set of valid words based on the current
      *  PATTERN. */
     public char getGuess(String pattern, List<Character> guesses) {
-        // TODO: Fill in this method.
-        return '?';
+        List<String> matchedWords;
+        matchedWords = keepOnlyWordsThatMatchPattern(pattern);
+        Map<Character, Integer> freqMap = getFreqMapThatMatchesPattern(matchedWords);
+        if (freqMap.isEmpty()) {
+            return '?';
+        }
+
+        for (Character guess : guesses) {
+            freqMap.remove(guess);
+        }
+
+        return Collections.max(freqMap.entrySet(), Map.Entry.comparingByValue()).getKey();
+    }
+
+    public Map<Character, Integer> getFreqMapThatMatchesPattern(List<String> matchedWords) {
+        List<Character> chars = new ArrayList<>();
+        Map<Character, Integer> frequency = new TreeMap<>();
+        int count;
+        for (String word : matchedWords) {
+            word.chars().forEach(y -> chars.add((char) y));
+        }
+
+        for (char alphabet = 'a'; alphabet <= 'z'; ++alphabet) {
+            if (!chars.contains(alphabet)) {
+                continue;
+            }
+            count = Collections.frequency(chars, alphabet);
+            frequency.put(alphabet, count);
+        }
+
+        return frequency;
+    }
+
+
+    public List<String> keepOnlyWordsThatMatchPattern(String pattern) {
+        Map<Integer, Character> justLetters = new TreeMap<>();
+        List<String> matchedPatternWords = new ArrayList<>();
+        for (int i = 0; i< pattern.length(); i++) {
+            if (Character.isLetter(pattern.charAt(i))) {
+               justLetters.put(i, (pattern.charAt(i)));
+            }
+        }
+        if (justLetters.isEmpty()) {
+            return words;
+        }
+
+        for ( String word : words) {
+            boolean match = false;
+            if (word.length() != pattern.length()){
+                continue;
+            }
+            for (Map.Entry<Integer, Character> entry : justLetters.entrySet()) {
+                match = word.charAt(entry.getKey()) == entry.getValue();
+                if(!match) {
+                    break;
+                }
+            }
+            if (match) {
+                matchedPatternWords.add(word);
+            }
+        }
+        return matchedPatternWords;
     }
 
     public static void main(String[] args) {
