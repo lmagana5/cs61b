b58b361615c7c6eee3c3f3ceb9f15b2504cb3722
lmagana5 <luis.a.magana1@gmail.com>
2023-09-04 20:15:48 -0700

    Complete RandomChooser.java

--- proj0/src/aoa/choosers/RandomChooser.java
+++ proj0/src/aoa/choosers/RandomChooser.java
@@ -1,32 +1,55 @@
 package aoa.choosers;
 
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdRandom;
 import aoa.utils.FileUtils;
+
+import java.util.*;
 
 public class RandomChooser implements Chooser {
     private final String chosenWord;
     private String pattern;
 
     public RandomChooser(int wordLength, String dictionaryFile) {
-        // TODO: Fill in/change this constructor.
-        chosenWord = "";
+        if (wordLength < 1) {
+            throw new IllegalArgumentException();
+        }
+        List<String> words = FileUtils.readWordsOfLength(dictionaryFile, wordLength);
+        if (words.isEmpty()) {
+            throw new IllegalStateException();
+        }
+        int numWords = words.size();
+        int randomlyChosenWordNumber = StdRandom.uniform(numWords);
+        chosenWord = words.get(randomlyChosenWordNumber);
+        pattern = "-".repeat(wordLength);
     }
 
     @Override
     public int makeGuess(char letter) {
-        // TODO: Fill in this method.
-        return 0;
+        int occurances = 0;
+        StringBuilder updatedPattern = new StringBuilder(pattern);
+        List<Integer> patternIndexes = new ArrayList<>();
+        for (int i = 0; i < chosenWord.length(); i++) {
+            if (chosenWord.charAt(i) == letter) {
+                occurances++;
+                patternIndexes.add(i);
+            }
+        }
+        for (int indexes : patternIndexes) {
+            updatedPattern.setCharAt(indexes, letter);
+        }
+        pattern = updatedPattern.toString();
+
+        return occurances;
     }
 
     @Override
     public String getPattern() {
-        // TODO: Fill in this method.
-        return "";
+        return pattern;
     }
 
     @Override
     public String getWord() {
-        // TODO: Fill in this method.
-        return "";
+        return chosenWord;
     }
 }
